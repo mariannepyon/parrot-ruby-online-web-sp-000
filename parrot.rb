108badf6a6e338b2a76ea699dbfed5eae8faa3df
Marianne Pyon <mariannepyon@gmail.com>
2019-07-31 17:09:15 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(speak = "Squawk!")
-  puts "#{speak}"
-else
-  puts "#{Pretty bird!}"
+  if puts "#{speak}"
+  else
+    puts "#{Pretty bird!}"
 end
 end
